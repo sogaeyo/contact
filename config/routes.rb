af65b12575e1397d452b402926aee4656876ea04
hspark <hspark@sogaeyo.com>
2015-07-24 10:01:46 +0000

    POST 방식으로 파라미터 받기

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 	get 'email/email_form' => 'email#email_form'
-	get 'email/email_send' => 'email#email_send'
+	post 'email/email_send' => 'email#email_send'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
