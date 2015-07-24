ef4df30088d5390331a7e96cf66902433a241cae
hspark <hspark@sogaeyo.com>
2015-07-24 10:15:21 +0000

    gmail 젬 추가 및 메일 전송 확인

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-
+gem 'gmail'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.3'
--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -3,7 +3,21 @@ class EmailController < ApplicationController
 	end
 
 	def email_send
-		@email = params[:email]
-		@content = params[:content]
+		email = params[:email]
+		title = params[:title]
+		content = params[:content]
+
+		gmail = Gmail.connect('hspark@sogaeyo.com', '@th0heez')
+		gmail.deliver do
+			charset = "UTF-8"
+			content_transfer_encoding="8bit"
+			to email
+			subject title
+			text_part do
+				body content
+			end
+		end
+
+		gmail.logout
 	end
 end
